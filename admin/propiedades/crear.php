706a2f4903ba2332432371fafe5b7fd6fcea830f
JohanCacerez <johancacerez10@gmail.com>
2024-02-16 17:35:11 -0600

    CRUD crear
    Se trabaja en la pagina para crear una nueva propiedad en venta

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,56 @@
+<?php
+require '../../includes/funciones.php';
+incluirTemplate('header')
+?>
+
+<main class="contenedor">
+  <h1>Crear</h1>
+
+  <a href="/admin" class="boton boton-verde">Volver</a>
+
+  <form action="" class="formulario">
+    <fieldset>
+      <legend>Informacion general</legend>
+
+      <label for="titulo">Titulo:</label>
+      <input type="text" id="titulo" placeholder="titulo propiedad">
+
+      <label for="precio">Precio:</label>
+      <input type="number" id="precio" placeholder="precio propiedad">
+
+      <label for="imagen">Imagen:</label>
+      <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+      <label for="descripcion">Descripcion:</label>
+      <textarea id="descripcion"></textarea>
+    </fieldset>
+
+    <fieldset>
+      <legend>Informacion propiedad</legend>
+
+      <label for="habitaciones">Habitaciones:</label>
+      <input type="number" id="habitaciones" placeholder="Ej: 3" min="1">
+
+      <label for="wc">Baños:</label>
+      <input type="number" id="wc" placeholder="Ej: 3" min="1">
+
+      <label for="estacionamiento">Estacionamiento:</label>
+      <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1">
+    </fieldset>
+
+    <fieldset>
+      <legend>Vendedor</legend>
+
+      <select>
+        <option value="1">Johan</option>
+        <option value="2">Ines</option>
+      </select>
+    </fieldset>
+
+    <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+  </form>
+
+</main>
+<?php
+incluirTemplate('footer');
+?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css" />
+    <link rel="stylesheet" href="/build/css/app.css" />
   </head>
   <body>
     <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
       <div class="contenedor contenido-header">
         <div class="barra">
           <a href="/">
-            <img src="build/img/logo.svg" alt="Logo" />
+            <img src="/build/img/logo.svg" alt="Logo" />
           </a>
 
           <div class="mobile-menu">
-            <img src="build/img/barras.svg" alt="icono menu" />
+            <img src="/build/img/barras.svg" alt="icono menu" />
           </div>
 
           <div class="derecha">
             <img
-              src="build/img/dark-mode.svg"
+              src="/build/img/dark-mode.svg"
               alt="boton dark mode"
               class="dark-mode-boton"
             />
