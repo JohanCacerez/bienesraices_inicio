d7ebf6b40c7785bf633e308cfe20bc611939722e
JohanCacerez <johancacerez10@gmail.com>
2024-03-05 19:30:45 -0600

    Guardado de datos
    Se añade una funcion que guarda los datos del formulario a pesar de estar mal, esto para no tener que volverlos a escribir

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,19 +6,21 @@ $db = conectarDB();
 //array mensajes de error
 $errores = [];
 
+$titulo = '';
+$precio = '';
+$descripcion = '';
+$habitaciones = '';
+$wc = '';
+$estacionamiento = '';
+$vendedores_id = '';
+
 //ejecutar el codigo despues de enviar formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   //echo "<pre>";
   //var_dump($_POST);
   //echo "</pre>";
 
-  $titulo = $_POST['titulo'];
-  $precio = $_POST['precio'];
-  $descripcion = $_POST['descripcion'];
-  $habitaciones = $_POST['habitaciones'];
-  $wc = $_POST['wc'];
-  $estacionamiento = $_POST['estacionamiento'];
-  $vendedores_id = $_POST['vendedor'];
+
 
   if (!$titulo) {
     $errores[] = "Debes añadir un titulo";
@@ -64,7 +66,7 @@ incluirTemplate('header')
 
   <a href="/admin" class="boton boton-verde">Volver</a>
 
-  <?php foreach($errores as $error): ?>
+  <?php foreach ($errores as $error) : ?>
     <div class="alerta error">
       <?php echo $error ?>
     </div>
@@ -75,29 +77,29 @@ incluirTemplate('header')
       <legend>Informacion general</legend>
 
       <label for="titulo">Titulo:</label>
-      <input type="text" id="titulo" name="titulo" placeholder="titulo propiedad">
+      <input type="text" id="titulo" name="titulo" placeholder="titulo propiedad" value="<?php echo $titulo; ?>">
 
       <label for="precio">Precio:</label>
-      <input type="number" id="precio" name="precio" placeholder="precio propiedad">
+      <input type="number" id="precio" name="precio" placeholder="precio propiedad" value="<?php echo $precio; ?>">
 
       <label for="imagen">Imagen:</label>
       <input type="file" id="imagen" accept="image/jpeg, image/png">
 
       <label for="descripcion">Descripcion:</label>
-      <textarea id="descripcion" name="descripcion"></textarea>
+      <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
     </fieldset>
 
     <fieldset>
       <legend>Informacion propiedad</legend>
 
       <label for="habitaciones">Habitaciones:</label>
-      <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1">
+      <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" value="<?php echo $habitaciones; ?>">
 
       <label for="wc">Baños:</label>
-      <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1">
+      <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" value="<?php echo $wc; ?>">
 
       <label for="estacionamiento">Estacionamiento:</label>
-      <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1">
+      <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" value="<?php echo $estacionamiento; ?>">
     </fieldset>
 
     <fieldset>
